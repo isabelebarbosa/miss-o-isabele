90f7f37be34861242ce54c9e696f1e42f90adf5f
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2024-06-04 12:18:12 -0300

    nhv

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPergunta = document.querySelector("caixa-pergunta");
+const caixaAlternativa = document.querySelector("caixa-alternativa");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const pergunta = [
+     {//primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+        alternativa:["alternativa1", "alternativa2"]
+     },
+    {//segundo objeto da lista de perguntas
+        enunciado: "pergunta2",
+        alternativa:["alternativa1", "alternativa2"]
+     },
+    
+
+]
