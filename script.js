59abc60b46f8e80e7c758906362e03c230f1ae97
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2024-06-17 10:58:34 -0300

    ihuygu

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector("texto-resultado");
 
 const pergunta = [
      {
-        enunciado: "como combater o dematamnto",
+        enunciado: "como combater o desmatamento",
         alternativa:[
             "reflorecimento de areas degradadas"
             "implementação de leis mais rigorosas para a aproteção das floretas"
